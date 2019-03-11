b8d02f1b6f0d85f0e30fc73babe5537258af8517
bernard <bernard@traveloka.com>
2019-03-11 21:13:32 +0700

    Update example/tsibstn/output.tf

--- examples/tsibstn/output.tf
+++ examples/tsibstn/output.tf
@@ -4,21 +4,36 @@ output "asg_bastion_name" {
 }
 
 output "sg_bastion_id" {
-  description = "The name of the security group for bastion instance"
+  description = "id of the security group for bastion instance"
   value       = "${module.this.sg_bastion_id}"
 }
 
 output "sg_postgres_id" {
-  description = "The name of the security group for bastion postgres"
+  description = "id of shared security group for postgres."
   value       = "${module.this.sg_postgres_id}"
 }
 
 output "sg_mongod_id" {
-  description = "The name of the security group for bastion mongod"
+  description = "id of shared security group for mongod."
   value       = "${module.this.sg_mongod_id}"
 }
 
 output "sg_mysql_id" {
-  description = "The name of the security group for bastion mysql"
+  description = "id of shared security group for mysql."
   value       = "${module.this.sg_mysql_id}"
+}
+
+output "sg_memcached_id" {
+  description = "id of shared security group for memcached."
+  value       = "${module.this.sg_memcached_id}"
+}
+
+output "sg_redis_id" {
+  description = "id of shared security group for redis."
+  value       = "${module.this.sg_redis_id}"
+}
+
+output "instance_role_name" {
+  description = "role name for the instances."
+  value       = "${module.this.instance_role_name}"
 }
